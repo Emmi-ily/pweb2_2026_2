ec0c3a6eea85c9bad0d04094e782515066d4b666
Emily Menezes <emily.m1@aluno.ifsc.edu.br>
2026-08-07 16:56:45 -0300

    a

--- pweb2_2026_2_/routes/web.php
+++ pweb2_2026_2_/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
+
+Route::get('/Aluno', [AlunoController::class, 'index']);
+Route::get('/Aluno/create', [AlunoController::class, 'create']);
+
+/*
+Route::get('/aluno', function () {
+    return view('aluno.list');
+    //return "<h3>aaaaaaa</h3>";
+});
+*/
