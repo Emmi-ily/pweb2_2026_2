6babcabbc7007033af6f6dc272565117ec9f1e60
Emily Menezes <emily.m1@aluno.ifsc.edu.br>
2026-08-14 17:12:33 -0300

    aula 14/08
    ta dando erro no deletar

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -13,9 +13,61 @@ class AlunoController extends Controller
 
         return view('aluno.list')->with(['dados' => $dados]);
     }
-    
+
     function create()
     {
         return view('aluno.form');
+    }
+
+
+    function validationForm(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'cpf' => 'required',
+        ], [
+            'nome.required' => "O :attribute é obrigatório",
+            'cpf.required' => "O :attribute é obrigatório"
+        ]);
+    }
+    function store(Request $request)
+    {
+        $this->validationForm($request);
+        Aluno::created($request->all());
+        return redirect('aluno')->with("success", 'Registro salvo com sucesso');
+    }
+
+
+    function edit($id)
+    {
+        $data = Aluno::find($id);
+        return view('aluno.form', compact('data')); //compact passa a referencia da memoria daqui para a view
+    }
+
+    function update(Request $request, $id)
+    {
+        $this->validationForm($request);
+        Aluno::find($id)->update($request->all());
+        return redirect('aluno')->with("success", 'Registro atualizado com sucesso');
+    }
+
+    function destroy($id)
+    {
+        Aluno::destroy($id);
+        return redirect('aluno')->with("success", 'Registro removido com sucesso!');
+    }
+
+    public function search (Request $request)
+    {
+        if(!empty($request->valor)) {
+            $dados = Aluno::where(
+                $request->tipo,
+                'like',
+                "%$request->valor%"
+            )->get();
+        }else {
+            $dados = Aluno::All();
+        }
+        return view('aluno.list', compact('dados'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,11 @@ Route::get('/', function () {
 
 Route::get('/aluno', [AlunoController::class, 'index']);
 Route::get('/aluno/create', [AlunoController::class, 'create']);
+Route::post('/aluno/store', [AlunoController::class, 'store'])->name('aluno.store'); //post pra não aparecer na url
+Route::post('/aluno/search', [AlunoController::class, 'search'])->name('aluno.search');
+Route::get('/aluno/edit/{id}', [AlunoController::class, 'edit'])->name('aluno.edit');
+Route::put('/aluno/{id}', [AlunoController::class, 'update'])->name('aluno.update');
+Route::put('/aluno/delete/{id}', [AlunoController::class, 'destroy'])->name('aluno.destroy');
 
 /*
 Route::get('/aluno', function () {
